038d908f1e76f95ca076fc206efebbb254d2c6f6
Akbar <n.akbarjon05@gmail.com>
2025-08-02 22:57:44 +0300

    api chamged

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
-const API_BASE = "http://localhost:5000";
+const API_BASE = "https://bookstore-app-tzol.onrender.com";
 
 export async function fetchBooks({ page, seed, region, likes, reviews }) {
-    const url = `http://localhost:5000/books?page=${page}&seed=${seed}&region=${region}&likes=${likes}&reviews=${reviews}`;
+    const url = `${API_BASE}/books?page=${page}&seed=${seed}&region=${region}&likes=${likes}&reviews=${reviews}`;
     const res = await fetch(url);
     return await res.json();
   }
